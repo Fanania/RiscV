c79e8b41b3b3ed70b38a474f019cf5e71ef04e9f
Fanania <fananiae30@gmail.com>
2026-01-26 17:42:25 +0200

    Update REG32x32.v
    Added an debug mode only for simulations

--- src/REG32x32.v
+++ src/REG32x32.v
@@ -15,11 +15,13 @@ module REG32x32 #(parameter ADDR_WIDTH = 32) (
   
   integer k,j;
   reg [`DATA_WIDTH-1:0] Reg_Cell [`ADDR_WIDTH-1:0];
-`ifdef SIMULATION_ON     /// -> not synth. used only for simulations:W
+`ifdef SIMULATION_ON
   initial begin
     for (k=0; k<`ADDR_WIDTH; k=k+1) begin
       Reg_Cell[k][`DATA_WIDTH-1:0] <= k;
     end
+      Reg_Cell[6][`DATA_WIDTH-1:0] <= {{(`DATA_WIDTH-2){1'b0}},2'b10};
+      Reg_Cell[5][`DATA_WIDTH-1:0] <= {{(`DATA_WIDTH-2){1'b1}},2'b10};      
   end
 `endif
   always @(posedge clk) begin 
@@ -38,5 +40,13 @@ module REG32x32 #(parameter ADDR_WIDTH = 32) (
    // Register 0 should be hardwired to 0 BOZO
     assign DataOutReg1[`DATA_WIDTH-1:0]=Reg_Cell[r_addr1[`ADDR_IDX-1:0]][`DATA_WIDTH-1:0];
     assign DataOutReg2[`DATA_WIDTH-1:0]=Reg_Cell[r_addr2[`ADDR_IDX-1:0]][`DATA_WIDTH-1:0];
+ // DEBUG FLORIN BOZO Remove me 
+  genvar i;
+  generate
+    for (i = 0; i < 32; i = i + 1) begin : expose_regfile
+      wire [31:0] RegCell_debug = Reg_Cell[i][`DATA_WIDTH-1:0];
+    end
+  endgenerate
 
+ // end debug mode
 endmodule
