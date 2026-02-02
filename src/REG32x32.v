9ad56a5d2f5bf7845e59205ac9a0b84e01ef65e3
Fanania <fananiae30@gmail.com>
2026-02-02 13:15:53 +0200

    Update REG32x32.v
    Added guards on simulation/debug specific structures

--- src/REG32x32.v
+++ src/REG32x32.v
@@ -17,18 +17,19 @@ module REG32x32 #(parameter ADDR_WIDTH = 32) (
   reg [`DATA_WIDTH-1:0] Reg_Cell [`ADDR_WIDTH-1:0];
 `ifdef SIMULATION_ON
   initial begin
-    for (k=0; k<`ADDR_WIDTH; k=k+1) begin
-      Reg_Cell[k][`DATA_WIDTH-1:0] <= k;
-    end
-      Reg_Cell[6][`DATA_WIDTH-1:0] <= {{(`DATA_WIDTH-2){1'b0}},2'b10};
-      Reg_Cell[5][`DATA_WIDTH-1:0] <= {{(`DATA_WIDTH-2){1'b1}},2'b10};      
+    $readmemh("/home/fananiae/disertatie_Anania/src/reg_init.hex", Reg_Cell);
+    Reg_Cell[0][`DATA_WIDTH-1:0] = {`DATA_WIDTH{1'b0}};  // Keep x0 hardwired to zero.
   end
 `endif
   always @(posedge clk) begin 
    if (rst) begin
+`ifndef SIMULATION_ON
        for (k=0; k<`ADDR_WIDTH; k=k+1) begin
          Reg_Cell[k][`DATA_WIDTH-1:0] <= {`ADDR_WIDTH{1'h0}};
        end
+`else
+      Reg_Cell[0][`DATA_WIDTH-1:0] <= {`DATA_WIDTH{1'b0}};
+`endif
     end else if (write_enable) begin
       // Se foloseste campul de adresa preluat din pachet pentru a determina unde se va peterce scrierea
       Reg_Cell[wr_addr[`ADDR_IDX-1:0]][`DATA_WIDTH-1:0] <= data_in[`DATA_WIDTH-1:0];
@@ -40,13 +41,15 @@ module REG32x32 #(parameter ADDR_WIDTH = 32) (
    // Register 0 should be hardwired to 0 BOZO
     assign DataOutReg1[`DATA_WIDTH-1:0]=Reg_Cell[r_addr1[`ADDR_IDX-1:0]][`DATA_WIDTH-1:0];
     assign DataOutReg2[`DATA_WIDTH-1:0]=Reg_Cell[r_addr2[`ADDR_IDX-1:0]][`DATA_WIDTH-1:0];
- // DEBUG FLORIN BOZO Remove me 
+
+`ifdef SIMULATION_ON 
   genvar i;
   generate
     for (i = 0; i < 32; i = i + 1) begin : expose_regfile
       wire [31:0] RegCell_debug = Reg_Cell[i][`DATA_WIDTH-1:0];
     end
   endgenerate
-
+`endif
  // end debug mode
+
 endmodule
